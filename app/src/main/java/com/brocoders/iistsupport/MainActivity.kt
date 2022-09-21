ed9ed8b78e7e7ffd969498adc026987aac3618ab
Luv Sahu <lksahuji365@gmail.com>
2022-09-21 22:07:58 +0530

    Initial commit

---
+++ app/src/main/java/com/brocoders/iistsupport/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.brocoders.iistsupport
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+    }
+}
