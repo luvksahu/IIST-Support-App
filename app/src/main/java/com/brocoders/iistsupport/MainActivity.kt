30cfc08e24d47c9e51711dbca9bb80bb07fb5e81
Luv Sahu <lksahuji365@gmail.com>
2022-09-22 15:42:33 +0530

    Login UI modified

--- app/src/main/java/com/brocoders/iistsupport/MainActivity.kt
+++ app/src/main/java/com/brocoders/iistsupport/MainActivity.kt
@@ -3,11 +3,14 @@ package com.brocoders.iistsupport
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.brocoders.iistsupport.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
     }
