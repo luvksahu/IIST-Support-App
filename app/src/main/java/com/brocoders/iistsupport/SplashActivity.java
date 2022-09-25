293a1e923c9720bd9d3eaa4ca55dda066e3e485c
Luv Sahu <lksahuji365@gmail.com>
2022-09-25 19:31:32 +0530

    Update SplashActivity

--- app/src/main/java/com/brocoders/iistsupport/SplashActivity.java
+++ app/src/main/java/com/brocoders/iistsupport/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
                     ex.printStackTrace();
                 }
                 finally{
-                    Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                    Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
                     startActivity(intent);
                     finish();
                 }
