4bce7c5730f9063712ffc58976b6b7ec73add068
luvksahu <lksahuji365@gmail.com>
2022-10-19 02:36:11 +0530

    Update

---
+++ app/src/main/java/com/brocoders/iistsupport/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.brocoders.iistsupport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.brocoders.iistsupport.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.btnFaculty.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, FacultyActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
