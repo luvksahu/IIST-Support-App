15f7a0e66bf8fdb76e65977e1a2581de1f6b25ef
ashish7477 <patidarashish173@gmail.com>
2022-10-31 23:39:28 +0530

    Kuch Layout Created

--- app/src/main/java/com/brocoders/iistsupport/busNo/BusNoActivity.java
+++ app/src/main/java/com/brocoders/iistsupport/busNo/BusNoActivity.java
@@ -32,12 +32,31 @@ public class BusNoActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.busRV);
         list = new ArrayList<>();
 
-        list.add(new BusModel("39","Mahesh Kumawat", "9754181983"));
-        list.add(new BusModel("2","Raju Pandey", "9846283006"));
-        list.add(new BusModel("3","Salman Khan", "9846283006"));
-        list.add(new BusModel("4","Karan Johar", "9846283006"));
-        list.add(new BusModel("5","Ananya Pandey", "9846283006"));
-        list.add(new BusModel("6","Saif Khan", "9846283006"));
+        list.add(new BusModel("36","Lokesh Yadav", "7869748081"));
+        list.add(new BusModel("41","Dinesh", "9754934409"));
+        list.add(new BusModel("34","Mukesh", "9171815344"));
+        list.add(new BusModel("52","Ghanshyam", "7869567620"));
+        list.add(new BusModel("44","Vijay Malviya", "9691185640"));
+        list.add(new BusModel("55","Santosh", "9424594601"));
+        list.add(new BusModel("28","Salim Bhopali", "9009792802"));
+        list.add(new BusModel("32","Mohan", "8827363998"));
+        list.add(new BusModel("53","Jayram Chouhan", "9977560726"));
+        list.add(new BusModel("29","Ghanshyam", "7869540199"));
+        list.add(new BusModel("27","Bharat", "9039261011"));
+        list.add(new BusModel("35","Sunil", "9826250407"));
+        list.add(new BusModel("54","Madhav", "9993432798"));
+        list.add(new BusModel("51","Anup", "9644009146"));
+        list.add(new BusModel("43","Mahesh", "9754181983"));
+        list.add(new BusModel("39","Ashok", "9661642960"));
+        list.add(new BusModel("25","Rajesh", "9993681204"));
+        list.add(new BusModel("48","Rahul Pawar", "9926081540"));
+        list.add(new BusModel("13","Hariom", "7999039349"));
+        list.add(new BusModel("33","Omprakash", "9826905808"));
+        list.add(new BusModel("31","Rajendra", "9179278274"));
+        list.add(new BusModel("30","Nitin Danke", "6264811081"));
+        list.add(new BusModel("38","Sachin", "9926678451"));
+        list.add(new BusModel("26","Sushil", "7869273927"));
+        list.add(new BusModel("37","Rahul Rao", "9165151573"));
 
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
